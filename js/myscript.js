e19f07207cb5251dc99357297202f4111fa3dae1
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-09-16 14:01:34 +0200

    inizializzazione e primi due step completati

---
+++ js/myscript.js
@@ -0,0 +1,32 @@
+// Stampare in pagina un item per ogni elemento contenuto in un array. OK!
+
+// Ogni item ha una icona "x" associata: cliccando su di essa, 
+// l'item viene rimosso dalla lista. OK!
+
+// Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio 
+// oppure ciccando su un pulsante, il testo digitato sarà aggiunto alla lista.
+
+
+
+
+
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+
+        listaSpesa : ["Pasta", "Dentifricio", "Patatine", "Carta Igienica", "Pesto", "Sapone",],
+
+        elementoListaAggiunto : "",
+    },  
+
+    methods: {
+
+        eliminaElemento : function(index){
+            this.listaSpesa.splice(index, 1);
+        },
+
+    },
+  });
+  
